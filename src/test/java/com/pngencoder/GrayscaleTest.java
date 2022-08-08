ec87589e2ec824981711a31c03d229cf6cf94957
Johan Tidén <johan.tiden@looklet.com>
2022-08-08 16:56:53 +0200

    Added failing tests

--- src/test/java/com/pngencoder/GrayscaleTest.java
+++ src/test/java/com/pngencoder/GrayscaleTest.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.Test;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GrayscaleTest {
     @Test
     void testGrayscaleGifShouldBeSavedProperly() throws IOException {
 
-        final BufferedImage bufferedImage = PngEncoderTestUtil.readTestImageResource("thermos_36667_sm.gif");
+        final BufferedImage bufferedImage = getRealGifImage();
 
         byte[] png = new PngEncoder().withBufferedImage(bufferedImage)
                 .toBytes();
@@ -28,6 +31,23 @@ public class GrayscaleTest {
         PngEncoderTestUtil.assertThatImageIsEqual(unpacked, bufferedImage);
     }
 
+    private BufferedImage getRealGifImage() {
+        return PngEncoderTestUtil.readTestImageResource("thermos_36667_sm.gif");
+    }
+
+    @Test
+    void showcaseMismatchInNumberOfChannels() {
+        final BufferedImage image = getRealGifImage();
+
+        WritableRaster imageRaster = image.getRaster();
+        final int channels = imageRaster.getSampleModel().getNumBands();
+
+        PngEncoderScanlineUtil.EncodingMetaInfo metaInfo = PngEncoderScanlineUtil.getEncodingMetaInfo(image);
+
+        assertEquals(channels, 1);
+        assertEquals(metaInfo.channels, 1); // actual is 3
+    }
+
     @Test
     void testGrayscaleSynthetic() throws IOException {
         BufferedImage bufferedImage = new BufferedImage(1, 2, 13);
@@ -37,7 +57,7 @@ public class GrayscaleTest {
         g.drawRect(0, 0, 1, 1);
 
         g.setColor(new Color(255, 255, 255));
-        g.drawRect(0, 0, 1, 1);
+        g.drawRect(0, 1, 1, 1);
 
 
         byte[] png = new PngEncoder().withBufferedImage(bufferedImage)
