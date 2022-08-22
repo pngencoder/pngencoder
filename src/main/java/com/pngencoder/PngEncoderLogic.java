d8bb77fcd4c5c9523e59ff653a7f63b574bbf436
Johan Tidén <johan.tiden@looklet.com>
2022-08-22 17:11:57 +0200

    Whitespace

--- src/main/java/com/pngencoder/PngEncoderLogic.java
+++ src/main/java/com/pngencoder/PngEncoderLogic.java
@@ -95,7 +95,7 @@ class PngEncoderLogic {
         countingOutputStream.write(FILE_BEGINNING);
 
         IndexedEncoderResult indexedEncoderResult = null;
-        if( bufferedImage.getType() == BufferedImage.TYPE_BYTE_INDEXED) {
+        if (bufferedImage.getType() == BufferedImage.TYPE_BYTE_INDEXED) {
             indexedEncoderResult = PngEncoderIndexed.encodeImageFromIndexed(bufferedImage, metaInfo);
         }
         else if (tryIndexedEncoding) {
